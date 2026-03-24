275d393f0fdcf86050823c77f817aded20c7d78f
vickhoff <dvickhoff@gmail.com>
2026-03-24 11:43:25 +0100

    Remove loginpage and signuppage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router"
-import { LoginPage } from "./assets/components/Authentication/LoginPage"
-import { SignupPage } from "./assets/components/Authentication/SignupPage"
-import { AuthLayout } from "./assets/components/Authentication/AuthLayout"
 import { LandingPage } from "./assets/components/LandingPage/LandingPage"
 import { AuthProvider } from "./context/AuthContext"
 import {ProfilePage} from "./assets/components/ProfilePage/ProfilePage"
@@ -15,10 +12,6 @@ function App() {
         <Route index element={<LandingPage />} />
         <Route path="login" element={<AuthenticationPage authType={"login"} />} />
         <Route path="signup" element={<AuthenticationPage authType={"signup"} />} />
-        <Route element={<AuthLayout/>}>
-          <Route path="signup" element={<SignupPage />} />
-          <Route path="login" element={<LoginPage />} />
-        </Route>
         <Route path="me" element={<ProfilePage />} />
         
 
